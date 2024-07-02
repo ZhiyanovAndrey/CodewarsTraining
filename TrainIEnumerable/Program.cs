5cea1603044cee09fdde1177b2d78d031a164231
Andrey <aazhiyanov@gmail.com>
2024-07-02 16:38:32 +0300

    ConvertToFooBar 2 вариант

--- TrainIEnumerable/Program.cs
+++ TrainIEnumerable/Program.cs
@@ -4,20 +4,20 @@ using System.Linq;
 Console.WriteLine("Hello, World!");
 
 
-var x = new[] { 7, 3, 15, 5 };
+var arr = new[] {3, 7, 15, 5 };
 
 
 //IEnumerable<string> x = null;
 
 
-foreach (var s in ConvertToFooBar(x))
+foreach (var s in ConvertToFooBar(arr))
 {
     Console.WriteLine(s);
     // должен вывести
-    // "foo"
-    // "7" 
-    // "foobar"
-    // "bar"
+    //3 - "foo"
+    //7 - "7" 
+    //15 - "foobar"
+    //5 - "bar"
 }
 
 
@@ -25,22 +25,23 @@ foreach (var s in ConvertToFooBar(x))
 static IEnumerable<string> ConvertToFooBar(IEnumerable<int> enumerable)
 {
 
-
-    var query = enumerable
-        .Where(x => x % 3 == 0).Select(x => "foo")
-        //.Where(x => x % 5 == 0).Select(x => "bar")
-        .Where(x => x % 15 == 0).Select(x => "foobar")
-        .Where(x => x % 3 != 0 && x=>x%5 != 0 $$ x => x % 15 != 0).Select(x => x.ToString());
+    var query = enumerable.Select(x =>
+    {
+        if (x % 15 == 0) return "foobar";
+        if (x % 5 == 0) return "bar";
+        if (x % 3 == 0) return "foo";
+        return x.ToString();
+    });
 
     return query;
 
-    //foreach (int a in enumerable)
-    //{
-    //    if (a % 15 == 0) yield return "foobar";
-    //    if (a % 5 == 0) yield return "bar";
-    //    if (a % 3 == 0) yield return "foo"; 
-    //    else yield return a.ToString();
-    //}
+    foreach (int a in enumerable)
+    {
+        if (a % 15 == 0) yield return "foobar"; //
+        if (a % 5 == 0) yield return "bar";
+        if (a % 3 == 0) yield return "foo";
+        else yield return a.ToString();
+    }
 }
 
 
