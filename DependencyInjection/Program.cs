15b95f448a48cc380ffd53942ab4fba558ea342d
ZhiyanovAndrey <aazhiyanov@gmail.com>
2024-08-05 16:19:44 +0300

    add project DependencyInjection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+IDataRepository repo = new DataRepository();
+BusinessLogic logic = new BusinessLogic(repo);
+logic.ProcessData("Example data");
+
+
+public interface IDataRepository
+{
+    void Save(string data);
+}
+
+public class DataRepository : IDataRepository
+{
+    public void Save(string data)
+    {
+        Console.WriteLine("Data saved: " + data);
+    }
+}
+
+public class BusinessLogic
+{
+    private readonly
+
+ IDataRepository _dataRepository;
+
+    // Constructor injection
+    public BusinessLogic(IDataRepository dataRepository)
+    {
+        _dataRepository = dataRepository;
+    }
+
+    public void ProcessData(string data)
+    {
+        _dataRepository.Save(data);
+    }
+}
+
+
---
+++ DiependancyInversion/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/* В этом примере зависимость IDataRepository 
+ * внедряется в BusinessLogic через конструктор, 
+ * что обеспечивает слабую связанность и упрощает тестирование компонентов.*/
+
+    IDataRepository repo = new DataRepository();
+    BusinessLogic logic = new BusinessLogic(repo);
+    logic.ProcessData("Example data");
+Console.ReadKey();
+
+
+
+public interface IDataRepository
+{
+    void Save(string data);
+}
+
+public class DataRepository : IDataRepository
+{
+    public void Save(string data) // принимает "Example data" 
+    {
+        Console.WriteLine("Data saved: " + data);
+    }
+}
+
+public class BusinessLogic
+{
+    private readonly
+
+ IDataRepository _dataRepository;
+
+    // Constructor injection
+    public BusinessLogic(IDataRepository dataRepository)
+    {
+        _dataRepository = dataRepository;
+    }
+
+    public void ProcessData(string data)
+    {
+        _dataRepository.Save(data); //DataRepository.Save передает "Example data"
+    }
+}
+
+
+
+
